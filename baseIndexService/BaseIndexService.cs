9ec0641ae5738993ac544bcae9b4008085b6b94f
Julia <juliayu9@gmail.com>
2016-06-14 21:51:30 -0700

    Update BaseIndexService.cs

--- baseIndexService/BaseIndexService.cs
+++ baseIndexService/BaseIndexService.cs
@@ -8,27 +8,26 @@ using System.Web;
 
 namespace Sabio.Web.Services.Index
 {
-    public abstract class BaseIndexService  //Connect to ElasticSearch 
+    public abstract class BaseIndexService 
     {
         private ElasticClient _Client { get; set; }
 
         protected ElasticClient GetClient()
         {
-            if (this._Client == null) //if client is null then do the connect 
+            if (this._Client == null) 
             {
                 var httpConnection = new AwsHttpConnection(new AwsSettings // using Elasticsearch.Net.Aws to connect because  its amazon service 
                 {
-                //  TODO: put all these values plus url into config files and load from here
-                    AccessKey = "AKIAIFSU4DDCF4AX7DEA",
-                    SecretKey = "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",
-                    Region = "us-west-2",
+                    AccessKey = ConfigService.ESAccessKey,
+                    SecretKey = ConfigService.ESSecretKey,
+                    Region = ConfigService.ESRegion,
                 });
                 var pool = new SingleNodeConnectionPool(new Uri(ConfigService.ESurl));
                 var config = new ConnectionSettings(pool, httpConnection);
                 this._Client  = new ElasticClient(config);
             }
 
-            return this._Client; // this make a new instace of client var client will only override the client 
+            return this._Client; 
         }
 
     }
